1e924ec1056093c856795ea130f10c2ed5f17711
AngelNifla <mnifla@unsa.edu.pe>
2021-07-31 21:57:58 -0500

    El asistente usa wikipedia para buscar informacion y alarma

--- Jueves.py
+++ Jueves.py
@@ -1,5 +1,6 @@
 import speech_recognition as sr
-import pyttsx3, pywhatkit
+import pyttsx3, pywhatkit, wikipedia, datetime, keyboard
+from pygame import mixer
 
 #VARIABLES
 
@@ -24,7 +25,7 @@ def listen():
         with sr.Microphone() as source:   #toma como fuente el microfono 
             print("escuchando...")
             pc = listener.listen(source)    #escucha lo que decimos
-            rec = listener.recognize_google(pc)     #convierte nuestra voz y la convierte a texto
+            rec = listener.recognize_google(pc, language="es")     #convierte nuestra voz y la convierte a texto
             rec = rec.lower()       #convierte el texto en minusculas
             if name in rec:         
                 rec = rec.replace(name,'')
@@ -35,12 +36,35 @@ def listen():
 
 #funcion principal
 def run():
-    rec = listen()
-    if 'reproduce' in rec:
-        music = rec.replace('reproduce','')
-        print("Reproduciendo"+ music)
-        talk("Reproduciendo"+ music)
-        pywhatkit.playonyt(music)
+    while True:
+        rec = listen()
+        #condicional para reconocer "reproduce"
+        if 'reproduce' in rec:
+            music = rec.replace('reproduce','')
+            print("Reproduciendo"+ music)
+            talk("Reproduciendo"+ music)
+            pywhatkit.playonyt(music)
+        #condicional para reconocer "busca"
+        elif 'busca' in rec:
+            search = rec.replace('busca','')
+            wikipedia.set_lang("es")        #la informacion se buscara en español
+            wiki = wikipedia.summary(search, 1)     #resume la informacion en una oración
+            print(search+ ": " +wiki)
+            talk(wiki)
+        #condicional para reconocer "alarma"
+        elif 'alarma' in rec:
+            alam = rec.replace('alarma','')
+            alam = alam.strip() 
+            talk("Alarma activada a las "+ alam +" horas")
+            while True:
+                if datetime.datetime.now().strftime('%H:%M') == alam:
+                    print("DESPIERTA!!!")
+                    mixer.init()
+                    mixer.music.load("alam.mp3")
+                    mixer.music.play()
+                    if keyboard.read_key() == "s":
+                        mixer.music.stop()
+                        break
 
 if __name__ == '__main__':
     run()
